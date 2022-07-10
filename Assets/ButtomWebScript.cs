56945ef72cfb0ebf64577f888cad87ec4a991576
johnny_programmer <c6jones720@yahoo.co.uk>
2022-07-10 19:55:51 +0100

    ray interactor is now working, buttons shall detect the ray being present and shall be clicked on trigger

---
+++ Assets/ButtomWebScript.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtomWebScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void DoClick()
+    {
+        Debug.Log("Web buttoin clicked");
+    }
+}
